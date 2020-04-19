e48755234ce06494178337f9f90a2eb664b07839
Francois Gaudin <francois@presencelearning.com>
2020-04-19 13:44:19 -0700

    start

--- protoconsole.py
+++ protoconsole.py
@@ -1,5 +1,39 @@
+from enum import IntEnum
+
 import krpc
-conn = krpc.connect(name='Hello World', address='192.168.1.195')
-vessel = conn.space_center.active_vessel
-print(vessel.name)
+import serial
+
+
+class Command(IntEnum):
+    # 0 - 63 -> command without value
+    HELLO = 0
+    BYE = 1
+    # 64 - 127 -> command with 8 bits flags value
+    FLAGS_1 = 64  # solar panel, gear, docked, lights, RCS, SAS, break, antenna
+    # 128 - 191 -> command with 16 bits value
+
+    # 192 - 255 -> command with 32 bits value
+    PERIAPSIS = 192  # 4 bytes value
+    APOAPSIS = 193  # 4 bytes value
+
+
+class Controller:
+    def __init__(self):
+        #self.kerbal = krpc.connect(name='protoconsole', address='192.168.1.195')
+        self.arduino = serial.Serial('/dev/ttyACM0', baudrate=115200)  # open serial port
+        print(self.arduino.name)  # check which port was really used
+        #vessel = self.kerbal.space_center.active_vessel
+        self.send_packet(Command.FLAGS_1.value)
+        self.arduino.close()  # close port
+
+    def send_packet(self, command, value=None):
+        self.arduino.write(command.to_bytes(1, 'big'))
+
+
+def main():
+    controller = Controller()
+
+
+if __name__ == '__main__':
+    main()
 
