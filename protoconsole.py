e8674e620054845c665111e75b3c3d73ea995fdf
Francois Gaudin <francois@gaudin.io>
2020-04-22 23:17:01 -0700

    Getting useful data from krpc

--- protoconsole.py
+++ protoconsole.py
@@ -15,30 +15,42 @@ class Command(IntEnum):
     # 1 - 63 -> command without value
     HELLO = 1
     BYE = 2
-    # 64 - 127 -> command with 8 bits flags value
-    FLAGS1 = 64  # solar panel, gear, docked, lights, RCS, SAS, brake, antenna
+    # 64 - 127 -> command with 8 bits value
+    FLAGS1 = 64  # flags for solar panel, gear, docked, lights, RCS, SAS, brake, antenna
+    TWR = 70
+    PITCH=71
     # 128 - 191 -> command with 16 bits value
 
     # 192 - 255 -> command with 32 bits value
     PERIAPSIS = 192  # 4 bytes value
     APOAPSIS = 193  # 4 bytes value
+    ALTITUDE = 194
+    VERTICAL_VELOCITY = 195
+    HORIZONTAL_VELOCITY = 196
 
 
 class Controller:
     def __init__(self):
-        #self.kerbal = krpc.connect(name='protoconsole', address='192.168.1.195')
+        self.kerbal = krpc.connect(name='protoconsole', address='127.0.0.1')
+        vessel = self.kerbal.space_center.active_vessel
         self.arduino = serial.Serial(PORT, baudrate=BAUD_RATE, timeout=TIMEOUT)  # open serial port
         print(self.arduino.name)  # check which port was really used
-        #vessel = self.kerbal.space_center.active_vessel
         if self.wait_for_board():
             print('Board connected')
         else:
             print("Failed to connect to board")
+
+        self.apoapsis = self.kerbal.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+        self.periapsis = self.kerbal.add_stream(getattr, vessel.orbit, 'periapsis_altitude')
+
         self.send_packet(Command.HELLO.value)
-        self.send_packet(Command.BYE.value)
-        self.send_packet(Command.FLAGS1.value, 0b01010101)
-        self.send_packet(Command.PERIAPSIS.value, 123456789)
+        self.loop()
         self.arduino.close()  # close port
+
+    def loop(self):
+        self.send_packet(Command.PERIAPSIS.value, int(self.periapsis()))
+        self.send_packet(Command.APOAPSIS.value, int(self.apoapsis()))
+        time.sleep(1)
 
     def wait_for_board(self):
         while True:
